203b0db162b066e74d6ec2705f5ce06c57a286a1
Jeffrey J. Early <jeffrey@jeffreyearly.com>
2015-01-05 08:07:19 -0800

    Initial Commit

---
+++ AnisotropicTurbulence/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AnisotropicTurbulence
+//
+//  Created by Jeffrey J. Early on 1/5/15.
+//  Copyright (c) 2015 Jeffrey J. Early. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
