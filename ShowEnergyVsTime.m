95ed290b97968996244a80550c8bf9d9ff4acf3a
JeffreyEarly <jeffrey@earlyinnovations.com>
2017-06-24 16:30:27 -0700

    Improving energy vs time script

--- ShowEnergyVsTime.m
+++ ShowEnergyVsTime.m
@@ -1,21 +1,24 @@
 % addpath('/Users/jearly/Dropbox/Documents/Matlab/jlab')
 % addpath('../GLOceanKit/Matlab/')
-file = '/Volumes/OceanTransfer/AnisotropicExperiments/StrongForcing/TurbulenceAnisotropic@x4.nc';
-% file = '/Volumes/OceanTransfer/AnisotropicExperiments/AnisotropicDataAdam/QGBetaPlaneTurbulenceFloats_experiment_04.nc';
+file = '/Volumes/OceanTransfer/AnisotropicExperiments/AnisotropicTurbulenceSpinUpModerateForcing.nc';
 
 g = 9.81;
 L_R = ncreadatt(file, '/', 'length_scale');
 
 [x,y,t] = FieldsFromTurbulenceFile( file, 0, 'x', 'y', 't');
 
-indices = 1:4:length(t);
-totalEnergy = zeros(length(indices),1);
-i = 1;
-for timeIndex=indices
+n = 20;
+indices = 1:ceil(length(t)/n):length(t);
+if indices(end) ~= length(t)
+    indices(end+1)=length(t);
+end
+
+totalEnergy = zeros(length(indices));
+for iIndex=1:length(indices)
+    timeIndex = indices(iIndex);
     [sshFD, k, l, f0] = FieldsFromTurbulenceFile( file, timeIndex, 'ssh_fd', 'k', 'l', 'f0');
     [kMag, energyMag] = EnergySpectrumFromSSH( sshFD, k, l, g, f0, L_R );
-    totalEnergy(i) = trapz(kMag,energyMag);
-    i = i+1;
+    totalEnergy(iIndex) = trapz(kMag,energyMag);
 end
 
 figure
